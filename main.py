f967b733f3794d893c196b772e2d8fee7459544a
arnavsomani <arnav.somani@nyu.edu>
2026-03-26 17:36:27 +0530

    update

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
 from dotenv import load_dotenv
+from importlib.metadata import version
 load_dotenv()
 
-from langchain_core import __version__ as core_version
-from langgraph import __version__ as lg_version
+
+core_version = version("langchain-core")
+lg_version = version("langgraph")
 from langchain_openai import ChatOpenAI
 
-core_version = version("langchian-core")
-lg_version = version("langgraph")
+
+print (f"langchain-core version: {core_version}")
+print (f"langchain version: {lg_version}")
 
 def main():
     print("Hello from langchain-langgraph!")
 
+    # Test OpenAI
+    llm = ChatOpenAI(model_name = "gpt-4o-mini", temperature = 0)
+    response = llm.invoke ("Say 'setup complete' in one word")
+    print (f"Response from ChatOpenAI: {response}")
+
+    print ("Setup Complete")
 
 if __name__ == "__main__":
     main()
